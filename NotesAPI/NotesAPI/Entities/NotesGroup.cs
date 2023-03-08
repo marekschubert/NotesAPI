b0a015ad904b0e435801a40043fc69fdf5ed7ef7
Marek Schubert <m.schubert@op.pl>
2023-03-08 23:45:39 +0100

    Note, NotesGroup, User entities added

---
+++ NotesAPI/NotesAPI/Entities/NotesGroup.cs
@@ -0,0 +1,13 @@
+﻿namespace NotesAPI.Entities
+{
+    public class NotesGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public GroupCategory GroupCategory { get; set; }
+
+
+
+
+    }
+}
